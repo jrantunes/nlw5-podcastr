f9e4e778735100c1c458762918cc551f64d34fed
José Neto <joseantunes2001@gmail.com>
2021-04-20 13:34:41 -0300

    component: _app stylization and Player component done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
-import { AppProps } from 'next/app'
-
 import '../styles/global.scss'
 
+import { AppProps } from 'next/app'
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import styles from '../styles/app.module.scss'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
 }
 
 export default MyApp
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import { Header } from "../components/Header";
 
 export default function Home() {
   return (
-    <Header/>
+    <p>index</p>
   )
 }
