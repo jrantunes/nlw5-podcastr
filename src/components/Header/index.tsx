a136e1ebcfbf79ad5b2dc7d48285acabe86b66ef
José Neto <joseantunes2001@gmail.com>
2021-04-20 12:50:51 -0300

    component: Header component done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,20 @@
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import styles from './styles.module.scss'
 
 export function Header() {
-  return (
-    <header>
-      <div>
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR,
+  })
 
-      </div>
+  return (
+    <header className={styles.headerContainer}>
+      <img src="/logo.svg" alt="Podcastr"/>
+
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
     </header>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,7 @@
+import { Header } from "../components/Header";
+
 export default function Home() {
-  return <h1>Hello</h1>
+  return (
+    <Header/>
+  )
 }
